98967ada0ba11e480aef527458f1e5b63dfbb298
cristobal <cridonoso@udec.cl>
2021-08-16 21:04:44 -0400

    xentropy tested

--- core/metrics.py
+++ core/metrics.py
@@ -5,12 +5,5 @@ from tensorflow.keras.metrics import categorical_accuracy
 @tf.function
 def custom_acc(y_true, y_pred):
     y_pred  = tf.nn.softmax(y_pred)[:,-1,:]
-
-
-    tf.print(y_pred)
-    tf.print(y_true)
     acc = categorical_accuracy(y_true, y_pred)
-
-    tf.print(acc)
-
     return tf.reduce_mean(acc)
