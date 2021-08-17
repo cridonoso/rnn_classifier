5130b740c4a0574bc90c34b9585bffaa4bcd0ef6
cristobal <cridonoso@udec.cl>
2021-08-16 21:03:37 -0400

    xentropy tested

--- core/metrics.py
+++ core/metrics.py
@@ -1,17 +1,16 @@
 import tensorflow as tf
 
+from tensorflow.keras.metrics import categorical_accuracy
+
 @tf.function
 def custom_acc(y_true, y_pred):
-    if len(tf.shape(y_pred)) > 2:
-        y_pred  = tf.nn.softmax(y_pred)[:,-1,:]
-    else:
-        y_pred  = tf.nn.softmax(y_pred)
+    y_pred  = tf.nn.softmax(y_pred)[:,-1,:]
 
-    y_true = tf.reshape(y_true, [-1, 1])
-    y_pred = tf.argmax(y_pred, 1, output_type=tf.int32)
-    y_pred = tf.expand_dims(y_pred, 1)
 
-    correct = tf.math.equal(y_true, y_pred)
-    correct = tf.cast(correct, tf.float32)
+    tf.print(y_pred)
+    tf.print(y_true)
+    acc = categorical_accuracy(y_true, y_pred)
 
-    return tf.reduce_mean(correct)
+    tf.print(acc)
+
+    return tf.reduce_mean(acc)
--- testing/losses.py
+++ testing/losses.py
@@ -4,13 +4,14 @@ import unittest
 from core.data import load_records
 from core.losses import custom_bce
 from core.models import get_lstm
+from core.metrics import custom_acc
 
 class TestStringMethods(unittest.TestCase):
 
     def test_xentropy(self):
 
         dataset, num_classes = load_records('./data/records/ztf/test',
-                                            batch_size=2,
+                                            batch_size=128,
                                             max_obs=200,
                                             take=1,
                                             return_num_classes=True)
@@ -21,7 +22,8 @@ class TestStringMethods(unittest.TestCase):
             y_pred = model(batch)
 
             bce = custom_bce(batch['label'], y_pred)
-
+            acc = custom_acc(batch['label'], y_pred)
+            print(acc)
         pass
 
 if __name__ == '__main__':
